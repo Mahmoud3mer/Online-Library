6a7656fd6cfc9769eb1a9ce686c746bc87d9fdfd
JS Project <mahmoudveteran@gmail.com>
2024-09-06 11:20:23 +0300

    tailwind-fontawsome-swiper

---
+++ Client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/pages/about/about.component.html',
+    './src/app/pages/book-details/book-details.component.html',
+    './src/app/pages/books/books.component.html',
+    './src/app/pages/cart/cart.component.html',
+    './src/app/pages/check-out/check-out.component.html',
+    './src/app/pages/contact/contact.component.html',
+    './src/app/pages/err404/err404.component.html',
+    './src/app/pages/home/home.component.html',
+    './src/app/pages/signin/signin.component.html',
+    './src/app/pages/signup/signup.component.html',
+    './src/app/pages/user-settings/user-settings.component.html',
+    './src/app/pages/wishlist/wishlist.component.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
