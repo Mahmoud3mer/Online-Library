9a5f1793cad0cd51b75f6174f700b33a4d6da313
MomenAladdin <moamenaladdin3@gmail.com>
2024-09-22 10:54:30 +0300

    userOrderList

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,29 +1,28 @@
-
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { BooksComponent } from './pages/books/books.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { CheckOutComponent } from './pages/check-out/check-out.component';
-import { WishlistComponent } from './pages/wishlist/wishlist.component';
-import { SigninComponent } from './pages/signin/signin.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { UserSettingsComponent } from './pages/user-settings/user-settings.component';
-import { BookDetailsComponent } from './pages/book-details/book-details.component';
-import { Err404Component } from './pages/err404/err404.component';
-import { AuthorsComponent } from './pages/authors/authors.component';
-import { EmailVerifiedComponent } from './pages/email-Verified/email-verified/email-verified.component';
-import { AccountSettingComponent } from './components/account-setting/account-setting.component';
-import { BooksGridListComponent } from './components/books-grid-list/books-grid-list.component';
-import { BooksListComponent } from './components/books-list/books-list.component';
-import { RecommendationComponent } from './pages/recommendation/recommendation.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { BooksComponent } from "./pages/books/books.component";
+import { CartComponent } from "./pages/cart/cart.component";
+import { CheckOutComponent } from "./pages/check-out/check-out.component";
+import { WishlistComponent } from "./pages/wishlist/wishlist.component";
+import { SigninComponent } from "./pages/signin/signin.component";
+import { SignupComponent } from "./pages/signup/signup.component";
+import { UserSettingsComponent } from "./pages/user-settings/user-settings.component";
+import { BookDetailsComponent } from "./pages/book-details/book-details.component";
+import { Err404Component } from "./pages/err404/err404.component";
+import { AuthorsComponent } from "./pages/authors/authors.component";
+import { EmailVerifiedComponent } from "./pages/email-Verified/email-verified/email-verified.component";
+import { AccountSettingComponent } from "./components/account-setting/account-setting.component";
+import { BooksGridListComponent } from "./components/books-grid-list/books-grid-list.component";
+import { BooksListComponent } from "./components/books-list/books-list.component";
+import { RecommendationComponent } from "./pages/recommendation/recommendation.component";
 import { ForgotPasswordComponent } from "./pages/forgot-password/forgot-password.component";
 import { ResetPasswordComponent } from "./pages/reset-password/reset-password/reset-password.component";
-import { StreamEventComponent } from './pages/stream-event/stream-event.component';
-import { SecuritySettingComponent } from './components/security-setting/security-setting.component';
+import { StreamEventComponent } from "./pages/stream-event/stream-event.component";
+import { SecuritySettingComponent } from "./components/security-setting/security-setting.component";
 import { PaymentComponent } from "./pages/payment/payment.component";
-
+import { MyOrdersComponent } from "./pages/my-orders/my-orders.component";
 
 export const routes: Routes = [
   {
@@ -61,15 +60,14 @@ export const routes: Routes = [
         pathMatch: "full",
       },
       {
-
-        path: 'grid',
+        path: "grid",
         component: BooksGridListComponent,
       },
       {
-        path: 'list',
+        path: "list",
         component: BooksListComponent,
-      }
-    ]
+      },
+    ],
   },
   {
     path: "check-out",
@@ -96,17 +94,18 @@ export const routes: Routes = [
     component: SignupComponent,
   },
   {
-    path: 'user-settings',
+    path: "user-settings",
     component: UserSettingsComponent,
     children: [
-      { path: '', redirectTo: 'account', pathMatch: 'full' },
-      { path: 'account', component: AccountSettingComponent },
-      { path: 'security', component: SecuritySettingComponent },
-    ]
+      { path: "", redirectTo: "account", pathMatch: "full" },
+      { path: "account", component: AccountSettingComponent },
+      { path: "security", component: SecuritySettingComponent },
+      { path: "my-orders", component: MyOrdersComponent },
+    ],
   },
   {
     path: "verify-email",
-    component: EmailVerifiedComponent
+    component: EmailVerifiedComponent,
   },
 
   {
