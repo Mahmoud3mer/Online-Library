a40a648c9bf1697e8893f1917d9e582aabb67a64
Ahmed-Gamal20 <93832798+Ahmed-Gamal20@users.noreply.github.com>
2024-09-26 03:12:57 +0300

    contact and about updated

--- Client/src/app/pages/about/about.component.ts
+++ Client/src/app/pages/about/about.component.ts
@@ -4,11 +4,12 @@ import { DarkModeService } from "../../services/dark-mode/dark-mode.service";
 import { PaymentComponent } from "../payment/payment.component";
 import { MyTranslateService } from "../../services/translation/my-translate.service";
 import { TranslateModule } from "@ngx-translate/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-about",
   standalone: true,
-  imports: [SubNavbarComponent, PaymentComponent, TranslateModule],
+  imports: [SubNavbarComponent, PaymentComponent, TranslateModule, RouterLink],
   templateUrl: "./about.component.html",
   styleUrl: "./about.component.scss",
 })
