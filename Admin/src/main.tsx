a2b17326274b50bac0bb2dcd5b607add34202ed7
JS Project <mahmoudveteran@gmail.com>
2024-09-28 15:13:52 +0300

    fixing admin bugs

--- Admin/src/main.tsx
+++ Admin/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './i18n';
 import App from './App';
 import './index.css';
 import './satoshi.css';
