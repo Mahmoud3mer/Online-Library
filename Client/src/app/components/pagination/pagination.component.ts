0ea3d13195d92bee24adf9261586cb16bc98ad41
JS Project <mahmoudveteran@gmail.com>
2024-09-30 19:13:51 +0300

    .

--- Client/src/app/components/pagination/pagination.component.ts
+++ Client/src/app/components/pagination/pagination.component.ts
@@ -5,48 +5,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-pagination',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <nav aria-label="Page navigation" class="flex justify-center mt-4">
-      <ul class="inline-flex items-center -space-x-px">
-        <li>
-          <button
-            [disabled]="currentPage === 1"
-            (click)="onPageChange(currentPage - 1)"
-            class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span class="sr-only">Previous</span>
-            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-          </button>
-        </li>
-        @for (page of pageNumbers; track page) {
-          <li>
-            <button
-              [class.bg-blue-50]="currentPage === page"
-              [class.text-blue-600]="currentPage === page"
-              [class.border-blue-300]="currentPage === page"
-              [class.hover:bg-blue-100]="currentPage === page"
-              [class.hover:text-blue-700]="currentPage === page"
-              [disabled]="page === '...'"
-              (click)="onPageChange(page)"
-              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            >
-              {{ page }}
-            </button>
-          </li>
-        }
-        <li>
-          <button
-            [disabled]="currentPage === totalPages"
-            (click)="onPageChange(currentPage + 1)"
-            class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span class="sr-only">Next</span>
-            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-          </button>
-        </li>
-      </ul>
-    </nav>
-  `
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss'
 })
 export class PaginationComponent {
   @Input() currentPage!: number;
